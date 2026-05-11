c796eac4b30597e81892e155dbbb36b6da64cc2e
RAgredaIpar <r.agredaipar@gmail.com>
2026-05-10 20:54:59 -0500

    feat: agregar microservicio security-server

---
+++ security-server/src/test/java/pe/rodrigo/securityserver/SecurityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.rodrigo.securityserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
